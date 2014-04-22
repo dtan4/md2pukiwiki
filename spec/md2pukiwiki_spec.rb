355c2a49c5df66042045436ad93ab9ec38fc90d6
dtan4 <dtanshi45@gmail.com>
2014-04-22 15:58:59 +0900

    Convert multi-line text

--- spec/md2pukiwiki_spec.rb
+++ spec/md2pukiwiki_spec.rb
@@ -25,5 +25,33 @@ describe Md2pukiwiki do
     it "should convert nested numbered list" do
       expect(Md2pukiwiki.convert("    1. list")).to eq "++ list"
     end
+
+    it "should convert multi-line text" do
+      markdown = <<-EOS.strip
+# header
+* list1
+* list2
+    * list2.1
+
+## subheader
+1. nlist1
+2. nlist2
+    1. nlist2.1
+      EOS
+
+      pukiwiki = <<-EOS.strip
+*header
+- list1
+- list2
+-- list2.1
+
+**subheader
++ nlist1
++ nlist2
+++ nlist2.1
+      EOS
+
+      expect(Md2pukiwiki.convert(markdown)).to eq pukiwiki
+    end
   end
 end
